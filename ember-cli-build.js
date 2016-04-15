c18ac2dbceaf59293452a6906b4357d852801d73
Adrien Bouttier <hibigbob@gmail.com>
2016-04-15 14:09:16 +0200

    Add bootstrap and font awesome

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,19 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+
+  app.import('bower_components/font-awesome/css/font-awesome.css');
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.ttf', {
+    destDir: 'fonts'
+  });
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff', {
+    destDir: 'fonts'
+  });
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff2', {
+    destDir: 'fonts'
+  });
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
