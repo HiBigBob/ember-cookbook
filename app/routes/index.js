5ba860622ece59777621402dfa9fb8d4b1ae3194
Adrien Bouttier <hibigbob@gmail.com>
2016-04-19 18:31:16 +0200

    Add authentification

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel(transition) {
+    if (!this.controllerFor('login').get('authenticate').get('userIsLoggedIn')) {
+      var loginController = this.controllerFor('login');
+      loginController.set('previousTransition', transition);
+      this.transitionTo('login');
+    }
+  }
 });
