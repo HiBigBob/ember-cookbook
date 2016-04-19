0f41465b0df0db47e087c1e019b8f3d7a93e2c81
Adrien Bouttier <hibigbob@gmail.com>
2016-04-19 18:32:41 +0200

    Add logout integration

--- app/components/header-navigation.js
+++ app/components/header-navigation.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	authenticate: Ember.inject.service('authentication'),
+	openDropDown: '',
+	router: Ember.inject.service('-routing'),
+
+	actions: {
+		toggleDropDown() {
+			if (this.get('openDropDown') === '') {
+				this.set('openDropDown', 'open');
+			} else {
+				this.set('openDropDown', '');
+			}
+		},
+
+		logout() {
+	        this.get('authenticate').logout();
+	        this.get('router').transitionTo('login');
+	    }
+	},
 });
