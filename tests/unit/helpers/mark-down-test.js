88b18e381a6ddeb95a20f7737c92cbf22711096b
Adrien Bouttier <hibigbob@gmail.com>
2016-05-02 21:06:10 +0200

    Add mark down helper

---
+++ tests/unit/helpers/mark-down-test.js
@@ -0,0 +1,10 @@
+import { markDown } from 'cookbook/helpers/mark-down';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | mark down');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = markDown([42]);
+  assert.ok(result);
+});
