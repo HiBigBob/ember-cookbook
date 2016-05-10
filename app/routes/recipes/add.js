a6053927fe89766a8d95c1165f6175bcca38106d
Adrien Bouttier <hibigbob@gmail.com>
2016-05-10 18:10:20 +0200

    Add security on add recipe route

--- app/routes/recipes/add.js
+++ app/routes/recipes/add.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	beforeModel(transition) {
+		if (!this.controllerFor('login').get('authenticate').get('userIsLoggedIn')) {
+			var loginController = this.controllerFor('login');
+			loginController.set('previousTransition', transition);
+  			this.transitionTo('login');
+		}
+	},
+
 	model: function() {
     	return Ember.RSVP.hash({
 			elements: this.store.findAll('element'),
