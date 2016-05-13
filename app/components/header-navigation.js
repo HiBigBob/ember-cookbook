888bd96d9a1a2564650808986176691581da66f9
Adrien Bouttier <hibigbob@gmail.com>
2016-05-13 22:42:13 +0200

    Add bookmark in header

--- app/components/header-navigation.js
+++ app/components/header-navigation.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	authenticate: Ember.inject.service('authentication'),
 	openDropDownUser: '',
+	openDropDownPin: '',
 	router: Ember.inject.service('-routing'),
+	bookmark: Ember.inject.service('service-bookmark'),
 
 	actions: {
 		toggleDropDownUser() {
