e2582e17c6ba673107be494c9c5080c02a316fa5
Adrien Bouttier <hibigbob@gmail.com>
2016-06-08 21:54:50 +0200

    Fix transition

--- app/services/service-bookmark.js
+++ app/services/service-bookmark.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Service.extend({
 	store: Ember.inject.service(),
 	routing: Ember.inject.service('-routing'),
+	previousTransition: null,
 	model: Ember.computed('store', function() {
 		return this.get('store').findAll('bookmark');
 	}),
@@ -18,9 +19,32 @@ export default Ember.Service.extend({
 
       	let self = this;
 		bookmark.save().then(function() {
-		  	self.get("routing").transitionTo("recipes.index");
+			if (self.get("previousTransition")) {
+				self.get("routing").transitionTo(self.get("previousTransition"));
+				self.set("previousTransition", null);
+			} else {
+				self.get("routing").transitionTo("recipes.index");	
+			}
 		}).catch(function(reason) {
 		  	console.log(reason);
 		});
+	},
+
+	removeBookmark(recipe) {
+		let bookmarks = this.get('store').peekAll('bookmark');
+		let bookmark = bookmarks.findBy('_recipe._id', recipe.get('id'));
+		bookmark.deleteRecord();
+		bookmark.save();
+	},
+
+	toggleBookmark(recipe) {
+		let model = this.get('store').peekRecord('recipe', recipe.get('id'));
+		model.set('bookmarked', !model.get('bookmarked'));
+		model.save();
+		if (model.get('bookmarked')) {
+			this.addBookmark(recipe);
+		} else {
+			this.removeBookmark(recipe);
+		}
 	}
 });
